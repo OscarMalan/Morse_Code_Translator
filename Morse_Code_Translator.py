ae3483f2ca9cd935c8c65b0304700dd2ad03e2a7
OscarMalan <oscarmalan@icloud.com>
2022-12-28 20:13:59 +0000

    Got final product working

--- Morse_Code_Translator.py
+++ Morse_Code_Translator.py
@@ -1,52 +1,58 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
-import numpy as np
 
 output_pin = 14
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(output_pin, GPIO.OUT)
 
 print("This turns text into morse code!!")
-message = input("Type your message please:  ")
+while True:
+    message = input("Type your message please:  ")
+    
+    # Dictionary representing the morse code chart
+    MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+	    				'C':'-.-.', 'D':'-..', 'E':'.',
+		    			'F':'..-.', 'G':'--.', 'H':'....',
+			    		'I':'..', 'J':'.---', 'K':'-.-',
+				    	'L':'.-..', 'M':'--', 'N':'-.',
+					    'O':'---', 'P':'.--.', 'Q':'--.-',
+    					'R':'.-.', 'S':'...', 'T':'-',
+	    				'U':'..-', 'V':'...-', 'W':'.--',
+		    			'X':'-..-', 'Y':'-.--', 'Z':'--..',
+			    		'1':'.----', '2':'..---', '3':'...--',
+				    	'4':'....-', '5':'.....', '6':'-....',
+					    '7':'--...', '8':'---..', '9':'----.',
+    					'0':'-----', ', ':'--..--', '.':'.-.-.-',
+	    				'?':'..--..', '/':'-..-.', '-':'-....-',
+		    			'(':'-.--.', ')':'-.--.-'}
 
-# Dictionary representing the morse code chart
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
-					'C':'-.-.', 'D':'-..', 'E':'.',
-					'F':'..-.', 'G':'--.', 'H':'....',
-					'I':'..', 'J':'.---', 'K':'-.-',
-					'L':'.-..', 'M':'--', 'N':'-.',
-					'O':'---', 'P':'.--.', 'Q':'--.-',
-					'R':'.-.', 'S':'...', 'T':'-',
-					'U':'..-', 'V':'...-', 'W':'.--',
-					'X':'-..-', 'Y':'-.--', 'Z':'--..',
-					'1':'.----', '2':'..---', '3':'...--',
-					'4':'....-', '5':'.....', '6':'-....',
-					'7':'--...', '8':'---..', '9':'----.',
-					'0':'-----', ', ':'--..--', '.':'.-.-.-',
-					'?':'..--..', '/':'-..-.', '-':'-....-',
-					'(':'-.--.', ')':'-.--.-'}
+    # Function to encrypt the string
+    # according to the morse code chart
+    cipher = ''
+    message = message.upper()
+    for letter in message:
+        if letter != ' ':
 
-# Function to encrypt the string
-# according to the morse code chart
-cipher = ''
-message = message.upper()
-for letter in message:
-    if letter != ' ':
+            # Looks up the dictionary and adds the
+            # corresponding morse code
+            # along with a space to separate
+            # morse codes for different characters
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            # 1 space indicates different characters
+            # and 2 indicates different words
+            cipher += ' '
 
-        # Looks up the dictionary and adds the
-        # corresponding morse code
-        # along with a space to separate
-        # morse codes for different characters
-        cipher += MORSE_CODE_DICT[letter] + ' '
-    else:
-        # 1 space indicates different characters
-        # and 2 indicates different words
-        cipher += ' '
-
-for element in range(0, len(cipher)):
-    if cipher[element] == '.':
-        print("dot")
-    elif cipher[element] == '-':
-        print("dash")
-    elif cipher[element] == ' ':
-        print('gap')
-    else:
-        print("space")
+    for element in range(0, len(cipher)):
+        if cipher[element] == '.':
+            GPIO.output(output_pin, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(output_pin, GPIO.LOW)
+        elif cipher[element] == '-':
+            GPIO.output(output_pin, GPIO.HIGH)
+            time.sleep(1.5)
+            GPIO.output(output_pin, GPIO.LOW)
+        elif cipher[element] == ' ':
+            time.sleep(1.5)
+        else:
+            time.sleep(3.5)
