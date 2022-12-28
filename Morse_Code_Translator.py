e18e30d5d742b15fdb657a66816792986111c546
Oscar Malan <96668033+OscarMalan@users.noreply.github.com>
2022-12-28 20:00:50 +0000

    just need to adapt the code for the pi

--- Morse_Code_Translator.py
+++ Morse_Code_Translator.py
@@ -1,89 +1,52 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 import numpy as np
 
 output_pin = 14
 
 print("This turns text into morse code!!")
-Message = input("Type your message please:  ")
+message = input("Type your message please:  ")
 
-Translation = {}
-x = 0
-Message = Message.lower()
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+					'C':'-.-.', 'D':'-..', 'E':'.',
+					'F':'..-.', 'G':'--.', 'H':'....',
+					'I':'..', 'J':'.---', 'K':'-.-',
+					'L':'.-..', 'M':'--', 'N':'-.',
+					'O':'---', 'P':'.--.', 'Q':'--.-',
+					'R':'.-.', 'S':'...', 'T':'-',
+					'U':'..-', 'V':'...-', 'W':'.--',
+					'X':'-..-', 'Y':'-.--', 'Z':'--..',
+					'1':'.----', '2':'..---', '3':'...--',
+					'4':'....-', '5':'.....', '6':'-....',
+					'7':'--...', '8':'---..', '9':'----.',
+					'0':'-----', ', ':'--..--', '.':'.-.-.-',
+					'?':'..--..', '/':'-..-.', '-':'-....-',
+					'(':'-.--.', ')':'-.--.-'}
 
-# Turning the text into Morse Code
-for letter in [*Message]:
-    if letter == 'a':
-        Translation.update({x: [1, 3]})
-    elif letter == 'b':
-        Translation.update({x: [3, 1, 1, 1]})
-    elif letter == 'c':
-        Translation.update({x: [3, 1, 3, 1]})
-    elif letter == 'd':
-        Translation.update({x: [3, 1, 1]})
-    elif letter == 'e':
-        Translation.update({x: [1]})
-    elif letter == 'f':
-        Translation.update({x: [1, 1, 3, 1]})
-    elif letter == 'g':
-        Translation.update({x: [3, 3, 1]})
-    elif letter == 'h':
-        Translation.update({x: [1, 1, 1, 1]})
-    elif letter == 'i':
-        Translation.update({x: [1, 1]})
-    elif letter == 'j':
-        Translation.update({x: [1, 3, 3, 3]})
-    elif letter == 'k':
-        Translation.update({x: [3, 1, 3]})
-    elif letter == 'l':
-        Translation.update({x: [1, 3, 1, 1]})
-    elif letter == 'm':
-        Translation.update({x: [3, 3]})
-    elif letter == 'o':
-        Translation.update({x: [3, 3, 3]})
-    elif letter == 'p':
-        Translation.update({x: [1, 3, 3, 1]})
-    elif letter == 'q':
-        Translation.update({x: [3, 3, 1, 3]})
-    elif letter == 'r':
-        Translation.update({x: [1, 3, 1]})
-    elif letter == 's':
-        Translation.update({x: [1, 1, 1]})
-    elif letter == 't':
-        Translation.update({x: [3]})
-    elif letter == 'u':
-        Translation.update({x: [1, 1, 3]})
-    elif letter == 'v':
-        Translation.update({x: [1, 1, 1, 3]})
-    elif letter == 'w':
-        Translation.update({x: [1, 3, 3]})
-    elif letter == 'x':
-        Translation.update({x: [3, 1, 1, 3]})
-    elif letter == 'y':
-        Translation.update({x: [3, 1, 3, 3]})
-    elif letter == 'z':
-        Translation.update({x: [3, 3, 1, 1]})
-    elif letter == ' ':
-        Translation.update({x: "space"})
+# Function to encrypt the string
+# according to the morse code chart
+cipher = ''
+message = message.upper()
+for letter in message:
+    if letter != ' ':
+
+        # Looks up the dictionary and adds the
+        # corresponding morse code
+        # along with a space to separate
+        # morse codes for different characters
+        cipher += MORSE_CODE_DICT[letter] + ' '
     else:
-        print("error")
-        exit()
-    x += 1
-    Translation.update({x: "wait"})
-    x += 1
+        # 1 space indicates different characters
+        # and 2 indicates different words
+        cipher += ' '
 
-x -= 1
-del Translation[x]
-
-print(len(Translation[4]))
-
-
-#for i in Translation:
-#    if np.searchsorted(Translation[i], 1):
-#        print("Dot")
-#    elif np.searchsorted(Translation[i], 3):
-#        print("Dash")
-#    else:
-#        print(Translation[i])
-
-
+for element in range(0, len(cipher)):
+    if cipher[element] == '.':
+        print("dot")
+    elif cipher[element] == '-':
+        print("dash")
+    elif cipher[element] == ' ':
+        print('gap')
+    else:
+        print("space")
